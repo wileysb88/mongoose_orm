cc0220a79bcdd281de19fa838d9f6b0afdfbee9a
Scott Wiley <wileysb@gmail.com>
2016-04-07 10:33:00 -0500

    setup of project and gitignore

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+// used to be in routes folder but changed to controllers
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
